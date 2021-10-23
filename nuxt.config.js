492796e946a1e013ce28a422744d06bc81980293
s10nishka <rockslon2@gmail.com>
2021-10-23 16:01:44 +0300

    add csv convert plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~plugins/vue-json-csv.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -41,6 +42,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+
   ],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
